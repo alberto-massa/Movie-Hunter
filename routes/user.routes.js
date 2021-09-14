f42f5e54fed2155ec4f9942698d193586efa733f
dany <dacr63@gmail.com>
2021-09-14 12:27:44 +0200

    added message system

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('./../models/User.model')
+const Message = require('./../models/Message.model')
 
 const { CDNupload } = require('../config/upload.config');
 const { default: axios } = require('axios');
+const { response } = require('express');
 
 router.get('/search', (req, res) => res.render('user/search'))
 
@@ -21,9 +23,8 @@ router.post('/search/', (req, res) => {
 router.get('/profile', (req, res) => {
 
     let user = req.session.currentUser
-
     console.log(user);
-    
+
     const object = {
         favouriteMovies : []
     }
@@ -32,20 +33,32 @@ router.get('/profile', (req, res) => {
         res.render('auth/login', {errorMsg: 'Please, login to check your profile'})
     } 
 
-    for (let i = 0; i < user.favouriteMovies.length; i++) {
-        axios
-        .get(`https://api.themoviedb.org/3/movie/${user.favouriteMovies[i]}?api_key=7d29ed24134deee78178561cf7b0a16c&language=en-US`)
-        .then(response => {
-            console.log(user.favouriteMovies);
-            object.favouriteMovies.push(response.data)
-            return User.findOne({ 'username': user.username })
-        })
+    console.log('------------FAVVVVVVVVVV---------------', user.favouriteMovies);
+    if (user.favouriteMovies.length > 0) {
+        for (let i = 0; i < user.favouriteMovies.length; i++) {
+            axios
+            .get(`https://api.themoviedb.org/3/movie/${user.favouriteMovies[i]}?api_key=7d29ed24134deee78178561cf7b0a16c&language=en-US`)
+            .then(response => {
+                object.favouriteMovies.push(response.data)
+                return User.findOne({ 'username': user.username })
+            })
+            .then(theUser => {
+                object.user = theUser
+                res.render('user/my-profile', object)
+            })
+            .catch(err => console.log(err))
+        }
+    } else {
+        console.log('ENTRANDO EN EL ELSEEEEEEEEEEEEEE');
+        console.log(user.username);
+        User
+        .findOne({ 'username': user.username })
         .then(theUser => {
-            object.user = theUser
-            res.render('user/my-profile', object)
+            res.render('user/my-profile', {theUser})
+            console.log(theUser)
         })
-        .catch(err => console.log(err))
     }
+
 })
 
 
@@ -70,5 +83,64 @@ router.post('/edit', CDNupload.single('avatar'), (req, res) => {
 
 })
 
+
+router.get('/messages', (req, res) => {
+
+    const user = req.session.currentUser
+    const messages = {
+        sentMessages: [],
+        receivedMessages: []
+    }
+
+    // sent messages
+    Message
+    .find({ authorId: user._id })
+    .populate('authorId')
+    .populate('receiverId')
+    .then((sentMsgs) => {
+        messages.sentMessages = sentMsgs
+        return Message
+        .find({ receiverId: user._id})
+        .populate('authorId')
+        .populate('receiverId')
+    })
+    .then((receivedMsgs) => {
+        messages.receivedMessages = receivedMsgs
+        res.render('user/messages', messages)
+    })
+})
+
+
+router.get('/sendmsg/:targetuser', (req, res) => {
+
+    const users = {
+        targetUser : req.params.targetuser,
+        user : req.session.currentUser
+    }
+    res.render('user/send-message', users)
+})
+
+
+router.post('/sendmsg/:targetuser', (req, res) => {
+
+    const user = req.session.currentUser
+    const targetUser = req.params.targetuser
+
+    const message = req.body
+
+    console.log(req.params);
+
+    User
+    .findOne({ username: targetUser })
+    .then(response => {
+        Message
+        .create({ authorId: user._id, receiverId: response._id, subject: message.subject, content: message.content })
+        .then(() => {
+            res.redirect('/user/profile')
+        })
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+})
 
 module.exports = router;
