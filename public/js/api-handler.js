a42f888f60eb5d12294808c969fbd7da79c6529a
dany <dacr63@gmail.com>
2021-09-16 13:30:02 +0200

    for pull

--- public/js/api-handler.js
+++ public/js/api-handler.js
@@ -3,15 +3,15 @@ const { default: axios } = require('axios');
 class CharactersApiHandler {
     constructor() {
         this.app = axios.create({
-            baseURL: 'https://api.themoviedb.org/3/movie'
+            baseURL: 'https://api.themoviedb.org/3'
         })
     }
 
-    getPopulars = () => this.app.get(`/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`)
-    getUpcoming = () => this.app.get(`/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&region=US`)
-    getDetails = movieId => this.app.get(`/${movieId}?api_key=${process.env.TMDB_KEY}&language=en-US`)
-    getFavourites = (...favourites) => this.app.get(`/${(favourites)}?api_key=${process.env.TMDB_KEY}&language=en-US`)
-
+    getPopulars = () => this.app.get(`/movie/popular?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`)
+    getUpcoming = () => this.app.get(`/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&region=US`)
+    getDetails = movieId => this.app.get(`/movie/${movieId}?api_key=${process.env.TMDB_KEY}&language=en-US`)
+    getFavourites = (...favourites) => this.app.get(`/movie/${(favourites)}?api_key=${process.env.TMDB_KEY}&language=en-US`)
+    getTrending = () => this.app.get(`/trending/movie/day?api_key=${process.env.TMDB_KEY}`)
 }
 
 module.exports = CharactersApiHandler
--- routes/base.routes.js
+++ routes/base.routes.js
@@ -8,15 +8,19 @@ router.get('/', (req, res) => {
     
     const popularMovies = apiHandler.getPopulars()
     const upcomingMovies = apiHandler.getUpcoming()
+    const trendingMovies = apiHandler.getTrending()
 
-    const movies = [popularMovies, upcomingMovies]
+    const movies = [popularMovies, upcomingMovies, trendingMovies]
 
     Promise
         .all(movies)
         .then(response => {
+            let randomNum = Math.floor((Math.random() * 20) + 1)
+
             const movieData = {
                 popularData: response[0].data,
-                upcomingData: response[1].data
+                upcomingData: response[1].data,
+                trendingData: response[2].data.results[randomNum]
             }
             res.render('index', {movieData})
         })
