f1c276d0be66de52e0b21eff9d13fadb1b20ec7f
Alberto Massa <massa.alberto@outlook.com>
2021-09-16 18:50:07 +0200

    wip3

--- public/js/glide.js
+++ public/js/glide.js
@@ -3,13 +3,9 @@ const gl1 = new Glide('#g1', {
   touchAngle: 45,
   focusAt: 1,
   startAt: 1,
-  perView: 4,
+  perView: 5,
   gap: 20,
   autoplay: 2000,
-  peek: {
-      before: 100,
-      after: 50
-  }
 }).mount()
 
 const gl2 = new Glide('#g2', {
@@ -17,11 +13,7 @@ const gl2 = new Glide('#g2', {
   touchAngle: 45,
   focusAt: 1,
   startAt: 1,
-  perView: 4,
+  perView: 5,
   gap: 20,
   autoplay: 2000,
-  peek: {
-      before: 100,
-      after: 50
-  }
 }).mount()
